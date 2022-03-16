4972ef21af505314c4dd0bc43425eae213301ed8
maliabarker <89502882+maliabarker@users.noreply.github.com>
2022-03-16 11:35:07 -0600

    styling and jinja loops

--- app.py
+++ app.py
@@ -4,23 +4,23 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', page_title='home')
 
 @app.route('/about', methods=['GET'])
 def about():
-    return render_template('about.html')
+    return render_template('about.html', page_title='about')
 
 @app.route('/skills', methods=['GET'])
 def skills():
-    return render_template('skills.html')
+    return render_template('skills.html', page_title='skills')
 
 @app.route('/blog', methods=['GET'])
 def blog():
-    return render_template('blog.html')
+    return render_template('blog.html', page_title='blog')
 
 @app.route('/resume', methods=['GET'])
 def resume():
-    return render_template('resume.html')
+    return render_template('resume.html', page_title='resume')
 
 if __name__ == '__main__':
     app.run(port=5002, debug=True)
