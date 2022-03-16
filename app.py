3edf212b6378727996ee04ec8f425b649285ce77
maliabarker <89502882+maliabarker@users.noreply.github.com>
2022-03-15 20:29:10 -0700

    adding html and js

--- app.py
+++ app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
+
+@app.route('/skills', methods=['GET'])
+def skills():
+    return render_template('skills.html')
+
+@app.route('/blog', methods=['GET'])
+def blog():
+    return render_template('blog.html')
+
+@app.route('/resume', methods=['GET'])
+def resume():
+    return render_template('resume.html')
+
 if __name__ == '__main__':
     app.run(port=5002, debug=True)
